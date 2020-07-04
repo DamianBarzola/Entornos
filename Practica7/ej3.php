8ddd86d6b73b69595feee95a361cc731d8878280
Damian <dbdamix@gmail.com>
2020-07-04 15:57:17 -0300

    ej3

--- Practica7/ej3.php
+++ Practica7/ej3.php
@@ -1,38 +1,23 @@
-<!--Crear un formulario que solicite la carga del nombre de usuario. Cuando se presione un botón
-crear una cookie para dicho usuario. Luego cada vez que ingrese al formulario mostrar el último
-nombre de usuario ingresado. -->
-
-<?php
-    if(isset($_POST["nombre"]))
-    {
-        $nombre = $_POST["nombre"];
-        setcookie("nombre", $nombre, time() + (60 * 60 * 24 * 90));
-    }else{
-        if (isset($_COOKIE["nombre"])){
-        $nombre = $_COOKIE["nombre"];
-    }
-    }
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN""http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<html>
-    <head>
-    <title>Nombres</title>
-    
-    </head>
-    <body>
-        <form action="ej3.php" method="post"> 
-        <br><br>
-        <h3>Ingrese un nombre:</h3>
-        <INPUT TYPE="TEXT" name="nombre" size="20" maxlength="20">        
-        <input type="submit" value="Aceptar">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
         <?php
-        if (empty($nombre)){
-            echo "<br><h2>Ningun nombre ingresado</h2>";
-        }else{
-            echo "<br><h2>El ultimo nombre ingresado fue " .$_COOKIE['nombre']. " </h2>";}
+        if(isset($_GET['nombre'])&&$_GET['nombre']!=""){
+            setcookie('nombreUsuario',$_GET['nombre'],time()+86400*365);         
+            echo "<label>Ultimo usuario ingresado: ".$_GET['nombre']."</label><br>";
+        }elseif(isset($_COOKIE['nombreUsuario'])){
+            echo "<label>Ultimo usuario ingresado: ".$_COOKIE['nombreUsuario']."</label><br>";
+        }
         ?>
-        </form>
-    </body>
-</html>
-
+        <label >Nombre de usuario</label>
+        <input name="nombre" type="text">
+        <button>Enviar</button>
+    </form>
+</body>
+</html>     
